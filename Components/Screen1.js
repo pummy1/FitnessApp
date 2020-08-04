5d8b46e776f0be3d3862811feb7ec221cfdf61bb
pummy sinha <pummy.sinha@zanjo.io>
2020-08-04 12:43:13 +0530

    component folder delete

--- Components/Screen1.js
+++
@@ -1,9 +0,0 @@
-/*Example of Navigation Drawer with Sectioned Menu*/
-import React, {Component} from 'react';
-import {StyleSheet} from 'react-native';
-import TabHelper from './TabHelper';
-export default class Screen1 extends Component {
-  render() {
-    return (<TabHelper />);
-  }
-}
